c4f7138f8e67d960c00ff84517a0fe3f0872a7cb
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-11-06 12:43:33 +0530

    Update practical2.py

--- practical2.py
+++ practical2.py
@@ -5,9 +5,8 @@ def is_prime(n):
    return False
   for i in range(2,int(n**0.5)+1):
     if n%i ==0:
-      return False
-  else:  
-     return True      
+      return False  
+ return True      
 n=int(input("enter a number"))
 if is_prime(n):
  print(f"{n} is prime")
