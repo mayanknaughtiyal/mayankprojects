a472f3d447fc4428cbaeac4ddbb8113c3d25a1d7
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-11-06 10:16:06 +0530

    Create practical2.py

---
+++ practical2.py
@@ -0,0 +1,16 @@
+#to write a progrrame to check if  number is prime,generate all prime numbers and generate first n prime numbers
+#to check if n is prime
+def is_prime(n):
+  if n <=1:
+   return False
+  for i in range(2,int(n**0.5)+1):
+    if n%i ==0:
+      return False
+    else:  
+     return True      
+n=int(input("enter a number"))
+if is_prime(n)==True:
+ print(f"{n} is prime")
+else:
+  print(f"{n} is not prime")
+  
