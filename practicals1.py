89f968549056422709b8d25ed5cdff771bd5d8ee
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-11-06 09:53:14 +0530

    Update practicals1.py

--- practicals1.py
+++ practicals1.py
@@ -1,6 +1,6 @@
 #pythont progrrame to find roots of a quadratic eauation
 #d=b^2-4ac
-|#x=(-b+-d^1/2)/2a
+#x=(-b+-d^1/2)/2a
 import cmath
 a= float(input("enter the cofficient a"))
 b= float(input("enter the cofficient b"))
@@ -8,4 +8,4 @@ c= float(input("enter THE COFFICIENT c"))
 d=b**2-4*a*c
 root1=(-b+cmath.sqrt(d))/2*a
 root2=(-b-cmath.sqrt(d))/2*a
-print("roots of the quadratic equation are"root1,root2)
+print("roots of the quadratic equation are",root1,"and",root2)
