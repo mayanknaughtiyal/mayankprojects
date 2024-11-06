8b40e002a2d83bc76e8d6110138dde0ac3a8aae1
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-11-06 12:09:16 +0530

    Update practical2.py

--- practical2.py
+++ practical2.py
@@ -13,3 +13,13 @@ if is_prime(n):
  print(f"{n} is prime")
 else:
   print(f"{n} is not prime")
+#now a programme to generate all prime numbers upto n
+def generate_prime_numbers_upto(n):
+ prime_no=[]
+ for i in range(2,n+1):
+     if is_prime(i):
+         prime_no.append(i)
+ return prime_no
+
+n=int(input("enter a number to generate prime numbers upto it"))
+print(f"prime numbers upto {n}:{generate_prime_numbers_upto(n)}")
